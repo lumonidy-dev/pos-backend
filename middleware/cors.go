f3f5eb5274d1727fe776af4d0702a604fa6da705
moisesnks <mleiva@utem.cl>
2024-04-07 01:41:39 -0400

    Refactor CORS middleware configuration to allow any origin, method, and header

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,33 +1,16 @@
 package middleware
 
-import (
-	"net/http"
-	"strings"
-)
+import "net/http"
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Configurar las opciones CORS
-		allowedOrigins := []string{"https://gpw6f5qg-8000.brs.devtunnels.ms"}
-		allowedMethods := []string{"GET", "POST", "OPTIONS"}
-		allowedHeaders := []string{"Content-Type"}
-
-		// Verificar si el origen de la solicitud está permitido
-		origin := r.Header.Get("Origin")
-		for _, allowedOrigin := range allowedOrigins {
-			if origin == allowedOrigin {
-				// Establecer los encabezados CORS
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-				w.Header().Set("Access-Control-Allow-Methods", strings.Join(allowedMethods, ","))
-				w.Header().Set("Access-Control-Allow-Headers", strings.Join(allowedHeaders, ","))
-
-				// Continuar con la siguiente middleware o handler
-				next.ServeHTTP(w, r)
-				return
-			}
-		}
-
-		// Si el origen no está permitido, responder con un error
-		http.Error(w, "Forbidden", http.StatusForbidden)
+		// Permitir cualquier origen
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		// Permitir cualquier método
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		// Permitir cualquier encabezado
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		// Continuar con el siguiente middleware
+		next.ServeHTTP(w, r)
 	})
 }
